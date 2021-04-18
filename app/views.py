760d6d1f5ce51b5f4054c0a01df0164c1c1f7242
zahed <zahed.zibaee2@gmail.com>
2021-04-18 19:53:28 +0430

    change list of customer

--- app/views.py
+++ app/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.views.generic import TemplateView
 from persiantools.jdatetime import JalaliDateTime
 
-from .models import Available, DeliveryFromCompany, DeliveryFromCategory, ReceivedFromCategory, ReceivedFromCompany
+from .models import Available, DeliveryToCompany, DeliveryToCategory, ReceivedFromCategory, ReceivedFromCompany
 
 
 class IndexView(TemplateView):
@@ -16,12 +14,29 @@ class IndexView(TemplateView):
             'count_available': Available.objects.count(), 
             'count_received_from_category': ReceivedFromCategory.objects.count(), 
             'count_received_from_company': ReceivedFromCompany.objects.count(), 
-            'count_received_from_category': DeliveryFromCategory.objects.count(), 
-            'count_received_from_company': DeliveryFromCompany.objects.count(), 
+            'count_received_from_category': DeliveryToCategory.objects.count(), 
+            'count_received_from_company': DeliveryToCompany.objects.count(), 
             }
         return render(request, 'app/index.html', context)
 
 
-class SearchView(TemplateView):
+class SearchView(LoginRequiredMixin, TemplateView):
+
     def get(self, request):
-        return render(request, 'app/search.html')
+        filter = request.GET.get('filter', '')
+        if filter == '':
+            available = Available.objects.all()
+            delivery_to_company = DeliveryToCompany.objects.all()
+            delivery_to_category = DeliveryToCategory.objects.all()
+            received_to_company = ReceivedFromCompany.objects.all()
+            received_to_category = ReceivedFromCategory.objects.all()
+        else:
+            pass
+        data = {
+            available: available,
+            delivery_to_company: delivery_to_company, 
+            delivery_to_category: delivery_to_category, 
+            received_to_company: received_to_company, 
+            received_to_category: received_to_category
+            }
+        return render(request, 'app/search.html', context = data)
